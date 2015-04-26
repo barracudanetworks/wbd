6d218d13395f8a37b0fa3a15aba0ba2e5f9000c3
John Maguire <contact@johnmaguire.me>
2015-04-26 01:57:51 -0400

    Add gorilla/mux router

--- web/web.go
+++ web/web.go
@@ -3,19 +3,26 @@ package web
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type indexHandler struct{}
+type IndexHandler struct{}
 
-func (ih *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (ih *IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("It works!"))
 }
 
 func Start(address string) {
-	ih := &indexHandler{}
-	http.Handle("/", ih)
+	r := mux.NewRouter()
+
+	// Routes
+	ih := &IndexHandler{}
+	r.Handle("/", ih)
+
+	// Register mux router to http /
+	http.Handle("/", r)
 
 	log.Print("Starting web server")
-
 	log.Fatal(http.ListenAndServe(address, nil))
 }
