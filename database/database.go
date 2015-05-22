5af88c44206a0d7b7544f9b0a7bf22d5caaa364d
John Maguire <contact@johnmaguire.me>
2015-05-22 03:30:34 -0400

    Add missing schema and DB helper functions

--- database/database.go
+++ database/database.go
@@ -23,7 +23,6 @@ CREATE INDEX client_id_index ON client_url_list (client_id);
 
 CREATE TABLE url_lists (
     id INTEGER PRIMARY KEY,
-    url_id INTEGER,
     name TEXT
 );
 
@@ -32,11 +31,26 @@ CREATE TABLE urls (
 	url TEXT
 );
 
+CREATE TABLE url_list_url (
+	id INTEGER PRIMARY KEY,
+	url_id INTEGER,
+	url_list_id INTEGER
+);
+
 `
 
 	sqlInsertUrl string = "INSERT INTO urls(url) VALUES(?);"
 	sqlFetchUrls string = "SELECT url FROM urls;"
 	sqlDeleteUrl string = "DELETE FROM urls WHERE url = ?;"
+
+	sqlFindListId string = "SELECT id FROM url_lists WHERE name = ?;"
+	sqlInsertList string = "INSERT INTO url_lists(name) VALUES(?);"
+	sqlFetchLists string = "SELECT name FROM url_lists;"
+	sqlDeleteList string = "DELETE FROM url_lists WHERE name = ?;"
+
+	sqlInsertListUrl string = "INSERT INTO url_list_url(url_list_id, url_id) VALUES(?, ?);"
+	sqlDeleteListUrl string = "DELETE FROM url_list_url WHERE id = ?;"
+	sqlFetchListUrls string = "SELECT * FROM url_list_url WHERE url_list_id = ?;"
 
 	sqlInsertConfig string = "INSERT INTO config(identifier, value) VALUES(?, ?);"
 )
@@ -60,28 +74,102 @@ func (db *Database) DeleteUrl(url string) (err error) {
 	return
 }
 
+func (db *Database) InsertList(name string) (err error) {
+	_, err = db.Conn.Exec(sqlInsertList, name)
+	return
+}
+
+func (db *Database) DeleteList(name string) (err error) {
+	_, err = db.Conn.Exec(sqlDeleteList, name)
+	return
+}
+
 func (db *Database) InsertConfig(identifier string, value string) (err error) {
 	_, err = db.Conn.Exec(sqlInsertConfig, identifier, value)
 	return
 }
 
+func (db *Database) FindListId(name string) (id int, err error) {
+	row := db.Conn.QueryRow(sqlFindListId, name)
+
+	err = row.Scan(&id)
+
+	return
+}
+
 func (db *Database) FetchUrls() (urls []string, err error) {
 	rows, err := db.Conn.Query(sqlFetchUrls)
-	defer rows.Close()
 	if err != nil {
 		return
 	}
+	defer rows.Close()
 
 	for rows.Next() {
 		var url string
+
 		err = rows.Scan(&url)
+		if err != nil {
+			return
+		}
+
 		urls = append(urls, url)
 	}
+
 	err = rows.Err()
 
 	return
 }
 
+func (db *Database) FetchLists() (lists []string, err error) {
+	rows, err := db.Conn.Query(sqlFetchLists)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+
+		err = rows.Scan(&name)
+		if err != nil {
+			return
+		}
+
+		lists = append(lists, name)
+	}
+
+	err = rows.Err()
+
+	return
+}
+
+func (db *Database) FetchListUrls(name string) (urls []string, err error) {
+	list_id, err := db.FindListId(name)
+	if err != nil {
+		return
+	}
+
+	rows, err := db.Conn.Query(sqlFetchListUrls, list_id)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var url string
+
+		err = rows.Scan(&url)
+		if err != nil {
+			return
+		}
+
+		urls = append(urls, url)
+	}
+
+	err = rows.Err()
+
+	return
+}
 func (db *Database) CreateTables() (err error) {
 	// Run SQL to create necessary schema
 	_, err = db.Conn.Exec(sqlCreateTables)
