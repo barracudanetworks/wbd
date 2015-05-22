e4e41f9a5c6e9f455c9a34ff8954374969ed373c
John Maguire <contact@johnmaguire.me>
2015-05-22 04:57:46 -0400

    Add database functions for dealing with clients

--- database/database.go
+++ database/database.go
@@ -3,23 +3,19 @@ package database
 import "database/sql"
 
 const (
+	// schema
 	sqlCreateTables string = `
 CREATE TABLE config (
-	identifier TEXT,
+	identifier TEXT NOT NULL,
 	value TEXT
 );
 
 CREATE TABLE clients (
-	identifier TEXT,
-	ip_address TEXT,
-	last_ping  INTEGER
+	identifier  TEXT NOT NULL,
+	ip_address  TEXT NOT NULL,
+	last_ping   TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	url_list_id INTEGER,
 );
-
-CREATE TABLE client_url_list (
-    client_id INTEGER,
-    url_list_id INTEGER
-);
-CREATE INDEX client_id_index ON client_url_list (client_id);
 
 CREATE TABLE url_lists (
     id INTEGER PRIMARY KEY,
@@ -39,16 +35,32 @@ CREATE TABLE url_list_url (
 
 `
 
+	// clients table
+	sqlInsertClient string = `
+	INSERT INTO clients (identifier, ip_address)
+	VALUES(?, ?);
+	`
+	sqlGetClient string = `
+	SELECT identifier, timestamp, ip_address, url_list_id
+	FROM clients WHERE identifier = ?;
+	`
+	sqlSetClientList      string = "UPDATE clients SET url_list_id = ? WHERE identifer = ?;"
+	sqlSetClientIpAddress string = "UPDATE clients SET ip_address = ? WHERE identifier = ?;"
+	sqlTouchClient        string = "UPDATE clients SET last_ping = CURRENT_TIMESTAMP;"
+
+	// urls table
 	sqlFindUrlId string = "SELECT id FROM urls WHERE url = ?;"
 	sqlInsertUrl string = "INSERT INTO urls(url) VALUES(?);"
 	sqlFetchUrls string = "SELECT url FROM urls;"
 	sqlDeleteUrl string = "DELETE FROM urls WHERE url = ?;"
 
+	// url_lists table
 	sqlFindListId string = "SELECT id FROM url_lists WHERE name = ?;"
 	sqlInsertList string = "INSERT INTO url_lists(name) VALUES(?);"
 	sqlFetchLists string = "SELECT name FROM url_lists;"
 	sqlDeleteList string = "DELETE FROM url_lists WHERE name = ?;"
 
+	// url_lists_url table
 	sqlInsertListUrl string = "INSERT INTO url_list_url(url_list_id, url_id) VALUES(?, ?);"
 	sqlDeleteListUrl string = "DELETE FROM url_list_url WHERE id = ?;"
 	sqlFetchListUrls string = `
@@ -64,8 +76,50 @@ type Database struct {
 	Conn *sql.DB
 }
 
+type Client struct {
+	identifier  string
+	timestamp   string
+	ip_address  string
+	url_list_id int
+}
+
 func (db *Database) Close() (err error) {
 	err = db.Conn.Close()
+	return
+}
+
+func (db *Database) InsertClient(identifier string, ip_address string) (err error) {
+	_, err = db.Conn.Exec(sqlInsertClient, identifier, ip_address)
+	return
+}
+
+func (db *Database) SetClientIpAddress(identifier string, ip_address string) (err error) {
+	_, err = db.Conn.Exec(sqlSetClientIpAddress, identifier, ip_address)
+	return
+}
+
+func (db *Database) TouchClient(identifier string) (err error) {
+	_, err = db.Conn.Exec(sqlTouchClient, identifier)
+	return
+}
+
+func (db *Database) AssignClientToList(name string, client_id string) (err error) {
+	list_id, err := db.FindListId(name)
+	if err != nil {
+		return
+	}
+
+	_, err = db.Conn.Exec(sqlSetClientList, list_id, client_id)
+	return
+}
+
+func (db *Database) GetClient(identifier string) (client Client, err error) {
+	err = db.Conn.QueryRow(sqlGetClient, identifier).Scan(
+		&client.identifier,
+		&client.timestamp,
+		&client.ip_address,
+		&client.url_list_id)
+
 	return
 }
 
