443206bc7c09597e9e5c4e754d5b803ee0a2a499
John Maguire <jmaguire@barracuda.com>
2016-07-28 16:15:03 -0400

    Bump version

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "wbd"
 	app.Usage = "take back control from your televisions"
-	app.Version = "0.1.0"
+	app.Version = "0.2.0"
 	app.Author = "John Maguire <jmaguire@barracuda.com>"
 
 	app.Commands = []cli.Command{
